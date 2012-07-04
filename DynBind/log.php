ddbc044357ac022d31edcd86388790ccc39a0d09
Lutz-Peter Hooge <lphooge@gmx.de>
2012-07-05 00:47:55 +0200

    buffer log messages if immediate writing is not possible

--- DynBind/log.php
+++ DynBind/log.php
@@ -11,6 +11,8 @@ class log {
 	public static $loglevel = 2;
 	public static $logfile = null;
 
+	protected static $buffer = array();
+
 	public static function setFile($logfile){
 		if(!file_exists($logfile) AND !touch($logfile)){
 			throw new Exception("cannot create logfile $logfile");
@@ -19,18 +21,29 @@ class log {
 			throw new Exception("logfile $logfile is not writable");
 		}
 		self::$logfile = $logfile;
+		self::clearbuffer();
 	}
 
 	public static function setLevel($loglevel){
 		self::$loglevel = (int) $loglevel;
 	}
 
+	protected static function clearbuffer(){
+		foreach(self::$buffer as $k=>$m){
+			self::write($m, self::$loglevel);
+			unset(self::$buffer[$k]);
+		}
+	}
+
 	public static function write($msg, $level=self::NORMAL){
 		if($level <= self::$loglevel){
 			if(self::$logfile){
 				@file_put_contents(self::$logfile, $msg."\n", FILE_APPEND);
-			} elseif($level >= self::CRITICAL){
-				error_log("DynBind critical error: ".$msg);
+			} else {
+				self::$buffer[] = $msg;
+				if($level >= self::CRITICAL){
+					error_log("DynBind critical error: ".$msg);
+				}
 			}
 		}
 	}
